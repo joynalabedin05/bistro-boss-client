99b477b28051ff4b16c0155a58c99d550fe35987
joynalabedin05 <joynal05101993@gmail.com>
2023-05-29 08:53:40 +0600

    added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -7,35 +7,50 @@ import Swal from "sweetalert2";
 
 const SignUp = () => {
     const navigate = useNavigate();
-    const {createUser, updateUserProfile} = useContext(AuthContext);
-    const { register, handleSubmit,reset, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data);
+        // console.log(data);
         createUser(data.email, data.password)
-        .then(result=>{
-            const createdUser = result.user;
-            console.log(createdUser);
-            updateUserProfile(data.name, data.photoURL)
-            .then(()=>{
-                console.log('user profile updated');
-                reset();
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'user profile updated',
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-                  navigate('/');
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                       const saveUser = {name: data.name, email: data.email}
+
+                        fetch('http://localhost:5000/users',{
+                            method:'POST',
+                            headers:{
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(saveUser)
+
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'user profile updated',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+                                }
+                            })
+
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
             })
-            .catch(error=>{
-                console.log(error);
+            .catch(error => {
+                console.log(error)
             })
-        })
-        .catch(error=>{
-            console.log(error)
-        })
-       
+
     };
 
     // console.log(watch("example"));

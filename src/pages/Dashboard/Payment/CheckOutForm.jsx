5fb4bcdbf0b188b13d5ef60ded5287811fad040d
joynalabedin05 <joynal05101993@gmail.com>
2023-06-03 11:45:25 +0600

    added

--- src/pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,11 +1,28 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect } from "react";
 import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ price }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const {user} = useAuth();
   const [cardError, setCardError] = useState();
+  const [axiosSecure] = useAxiosSecure();
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState('');
+
+  useEffect(() => {
+    axiosSecure.post('/create-payment-intent', { price })
+      .then(res => {
+        // console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret)
+      })
+  }, []);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!stripe || !elements) {
@@ -16,7 +33,7 @@ const CheckOutForm = () => {
       return
     }
     // console.log('card', card);
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
+    const { error, paymentMethod} = await stripe.createPaymentMethod({
       type: 'card',
       card
     });
@@ -29,7 +46,32 @@ const CheckOutForm = () => {
       console.log('payment method', paymentMethod);
     }
 
+    setProcessing(true);
+    const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: user?.displayName || 'annonymous',
+            email: user?.email || 'unknown',
+          },
+        },
+      },
+    );
+
+    if(confirmError){
+      console.log(confirmError);
+    }
+    console.log('payment intent',paymentIntent);
+    setProcessing(false);
+    if(paymentIntent.status === "succeeded"){
+      setTransactionId(paymentIntent.id);
+      // todo next <steps
+    }
+
   }
+
+
   return (
     <div>
       <form className="w-2/3 m-7" onSubmit={handleSubmit}>
@@ -49,12 +91,15 @@ const CheckOutForm = () => {
             },
           }}
         />
-        <button className="btn btn-outline btn-primary mt-4" type="submit" disabled={!stripe}>
+        <button className="btn btn-primary mt-4" type="submit" disabled={!stripe || !clientSecret || processing}>
           Pay
         </button>
       </form>
       {
-        cardError&& <p className="text-red-500 ml-4">{cardError}</p>
+        cardError && <p className="text-red-500 ml-4">{cardError}</p>
+      }
+      {
+        transactionId && <p className="text-green-500 ml-4">Transaction complete with transaction Id: {transactionId}</p>
       }
     </div>
   );
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,16 +2,21 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 import CheckOutForm from "./CheckOutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/UseCart";
 
 // todo provide publish able key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway);
+// console.log(import.meta.env.VITE_Payment_Gateway);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item)=> sum+item.price, 0);
+    const price = parseFloat(total.toFixed(2));
     return (
         <div>
             <SectionTitle subHeading='please provide' heading='Payment'></SectionTitle>
            <h4 className="class text-3xl">taka taka tumi uira uira aso </h4>
            <Elements stripe={stripePromise}>
-                <CheckOutForm></CheckOutForm>
+                <CheckOutForm price={price}></CheckOutForm>
             </Elements> 
            
         </div>
